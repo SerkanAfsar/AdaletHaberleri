5168d3843e9f30022e77b3aacf2086e3b7614551
SerkanAfsar <serkan-afsar@hotmail.com>
2025-07-05 13:44:03 +0300

    Api Cron Update

---
+++ app/api/refetchjob/route.ts
@@ -0,0 +1,11 @@
+import { envVariables } from "@/Utils";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const response = await fetch(envVariables.NEXT_PUBLIC_BASE_URL);
+  console.log(response);
+
+  return NextResponse.json({ message: "success" }, { status: 200 });
+}
+
+export const dynamic = "force-dynamic";
